16ad88aca159e43ca1c1baa901d0f0aa5599c0e4
linchong <714987173@qq.com>
2019-04-19 16:42:05 +0800

    rabbitmq（点对点、多播）

--- springboot-01-cache/src/main/java/com/atguigu/cache/config/MyRedisConfig.java
+++ springboot-01-cache/src/main/java/com/atguigu/cache/config/MyRedisConfig.java
@@ -9,13 +9,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
-import org.springframework.stereotype.Component;
 
 import java.net.UnknownHostException;
 import java.time.Duration;
--- springboot-02-amqp/src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
+++ springboot-02-amqp/src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
@@ -1,16 +1,50 @@
 package com.atguigu.amqp;
 
+import com.atguigu.amqp.entity.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot02AmqpApplicationTests {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
+    /**
+     * 点对点
+     */
     @Test
     public void contextLoads() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("msg","hello");
+        map.put("data", Arrays.asList("my precious",true,123));
+        //rabbitTemplate.convertAndSend("exchange.direct","atguigu.emps",map);
+        rabbitTemplate.convertAndSend("exchange.direct","atguigu.emps",new Book("八十天环游地球",100.2));
     }
+
+    @Test
+    public void receive() {
+        Object o = rabbitTemplate.receiveAndConvert("atguigu.emps");
+        System.out.println(o.getClass());
+        System.out.println(o);
+    }
+
+    /**
+     * 多播
+     */
+    @Test
+    public void test1() {
+        rabbitTemplate.convertAndSend("exchange.fanout","",new Book("三国演义",99.99));
+    }
+
+
 
 }
